a0817978245a62ac3786feb5428eebb75befd73f
SACHINKATHAR2005 <sachin.kathar@mit.asia>
2026-04-01 21:24:30 +0530

    all error fixed

--- client/lib/api.ts
+++ client/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:5001/api',
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://nanobill-oc26.onrender.com/api",
   // Critical for JWT verification! Allows Node to read/write auth cookies securely.
   withCredentials: true,
   headers: {
